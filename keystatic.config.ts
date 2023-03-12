495c1f2bef2399d992f3ef38f8c9ec8d3a8e66b1
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-03-12 16:30:34 +1300

    added keystatic

---
+++ keystatic.config.ts
@@ -0,0 +1,58 @@
+import {
+  collection,
+  config,
+  fields,
+  GitHubConfig,
+  LocalConfig,
+  singleton,
+} from "@keystatic/core";
+
+const storage: LocalConfig["storage"] | GitHubConfig["storage"] =
+  process.env.NODE_ENV === "development"
+    ? { kind: "local" }
+    : {
+        kind: "github",
+        repo: {
+          owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
+          name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
+        },
+      };
+
+export default config({
+  storage,
+  singletons: {
+    index: singleton({
+      label: "Home",
+      path: "content/pages/home/",
+      schema: {
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+        }),
+      },
+    }),
+  },
+  collections: {
+    posts: collection({
+      label: "Posts",
+      path: "content/posts/*/",
+      slugField: "title",
+      schema: {
+        title: fields.slug({
+          name: {
+            label: "Title",
+          },
+        }),
+        summary: fields.text({
+          label: "Summary",
+          validation: { length: { min: 4 } },
+        }),
+        publishedDate: fields.date({ label: "Published Date" }),
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+        }),
+      },
+    }),
+  },
+});
