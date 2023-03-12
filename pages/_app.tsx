3d2d51534e001bb5ecffb6150293e5069b8c9624
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-03-12 16:15:49 +1300

    added tailwind and layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={`${inter.className} grid h-screen`}
+      style={{
+        gridTemplate: "'a b' 100vh / 18rem 1fr",
+      }}
+    >
+      <nav className="p-9 overflow-auto">
+        <h1 className="pb-9">Artist name</h1>
+        <ul>
+          <li>hello</li>
+          <li>
+            <button>test</button>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
